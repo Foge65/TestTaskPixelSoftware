d962f758afddaeb7d8c9d94d7ef5f4b018755f02
fog <mr.texnik666@gmail.com>
2025-05-29 15:21:21 +0300

    main

---
+++ src/main/java/software/pxel/TestPioneerApplication.java
@@ -0,0 +1,13 @@
+package software.pxel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPioneerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPioneerApplication.class, args);
+    }
+
+}
